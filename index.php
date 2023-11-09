17e11e9326e4231f5fdca724cd4f42d8dbcb17a8
eesuhn <102596628+eesuhn@users.noreply.github.com>
2023-11-09 17:52:22 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * ReNew - A sustainable marketplace that empowers the community to participate in recycling 
+ * by providing a platform for both the public to responsibly recycle items and for local 
+ * artists to showcase and sell their unique creations made from recycled materials.
+ * 
+ * @package ReNew
+ * @version 0.0.1
+ * @author 
+ * - github.com/eesuhn
+ * - github.com/shai-mohan
+ * 
+ * @dependencies 
+ * [Backend]
+ * - PHP 8.1.10
+ * - MySQL Ver 15.1 Distrib 10.4.25-MariaDB
+ * - jQuery 3.5.1
+ * 
+ * [Frontend]
+ * - Bootstrap 4.5.2
+ * - PopperJS v2.11.6
+ * - Font Awesome 5.15.3
+ * - Google Fonts Roboto
+ */
