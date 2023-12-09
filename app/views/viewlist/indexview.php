e98a726dd78d4436d66103309b1d486211ecd8ab
eesuhn <102596628+eesuhn@users.noreply.github.com>
2023-12-09 19:04:34 +0800

    Improved indexView

--- app/views/viewlist/indexview.php
+++ app/views/viewlist/indexview.php
@@ -35,6 +35,7 @@ HTML;
 $body .= <<<HTML
     <div class="hero-image">
         <img src="$root/app/assets/public/hero.png" alt="Hero Image" class="hero-img">
+        <div class="overlay"></div>
         <div class="carousel-caption1">
             <h3>Reaching for global sustainability</h3>
             <p>ReNew, is a pioneering eco-conscious company on a mission to redefine sustainability and foster community engagement in recycling.</p>
@@ -79,6 +80,7 @@ $body .= <<<HTML
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="$root/app/assets/public/carousel1.jpg" class="d-block w-100" alt="...">
+                <div class="overlay"></div>
                 <div class="carousel-caption">
                     <h5>Get reward points by recycling!</h5>
                     <p>A sustainable marketplace that promotes the circular economy, reduces waste, and showcases the potential of repurposed materials.</p>
@@ -86,6 +88,7 @@ $body .= <<<HTML
             </div>
             <div class="carousel-item">
                 <img src="$root/app/assets/public/carousel2.jpg" class="d-block w-100" alt="...">
+                <div class="overlay"></div>
                 <div class="carousel-caption">
                     <h5>Get reward points by recycling!</h5>
                     <p>A sustainable marketplace that promotes the circular economy, reduces waste, and showcases the potential of repurposed materials.</p>
@@ -93,6 +96,7 @@ $body .= <<<HTML
             </div>
             <div class="carousel-item">
                 <img src="$root/app/assets/public/carousel3.jpg" class="d-block w-100" alt="...">
+                <div class="overlay"></div>
                 <div class="carousel-caption">
                     <h5>Get reward points by recycling!</h5>
                     <p>A sustainable marketplace that promotes the circular economy, reduces waste, and showcases the potential of repurposed materials.</p>
@@ -100,10 +104,11 @@ $body .= <<<HTML
             </div>
         </div>
     </div>
-    <div class="row">
+    <div class="row rec-btn-row">
         <div class="col">
             <div class="center-text3">
-                <h1 class="">Recycle now to save the planet!</h1>
+                <h1 class="rec-title">Recycle now to</h1>
+                <h1 class="rec-title">save the planet!</h1>
             </div>
         </div>
         <div class="col">
