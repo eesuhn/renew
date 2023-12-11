51206f8536d474469d09e4d5e09d5f4dc90210ea
shai-mohan <shailla.mohan@gmail.com>
2023-12-11 23:14:12 +0800

    FE created orders page

---
+++ app/views/js/ordersview.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    var data = [
+        {
+            "recycled": "Bird House, Upcycled Glass Vases,...",
+            "date&time": "2023-12-11 10:30:00",
+            "total": "300",
+            "status": "Completed",
+            "office": "Edinburgh",
+            "extn": "5421"
+        },
+        {
+            "recycled": "Bird House, Upcycled Glass Vases,...",
+            "date&time": "2023-12-11 10:30:00",
+            "total": "20",
+            "status": "Processing",
+            "office": "Edinburgh",
+            "extn": "8422"
+        },
+        {
+            "recycled": "Bird House, Upcycled Glass Vases,...",
+            "date&time": "2023-12-11 10:30:00",
+            "total": "0",
+            "status": "Cancelled",
+            "office": "London",
+            "extn": "2323"
+        }
+    ];
+
+    $('#order-history').DataTable({
+        data: data,
+        columns: [
+            { data: 'recycled' },
+            {
+                data: 'date&time',
+                render: function (data, type, row) {
+                    return '<span>' + data + '</span>';
+                }
+            },
+            {
+                data: 'status',
+                render: function (data, type, row) {
+                    var badgeClass = "";
+                    var statusText = "";
+
+                    if (data === "Cancelled") {
+                        badgeClass = "badge badge-danger";
+                        statusText = "Cancelled";
+
+                    } else if (data === "Completed") {
+                        badgeClass = "badge badge-success";
+                        statusText = "Completed";
+
+                    } else if (data === "Processing") {
+                        badgeClass = "badge badge-warning";
+                        statusText = "Processing";
+                    }
+
+                    return '<span class="' + badgeClass + '">' + statusText + '</span>';
+                }
+            },
+            { data: 'total' },
+            {
+                data: 'null',
+                render: function (data, type, row) {
+                    return '<span class="ellipsis" style="color: black !important;"><a href="#" style="color: black !important;"><i class="fas fa-ellipsis-h"></i></a></span>';
+
+                }
+            }
+
+        ]
+    });
+});
---
+++ app/views/viewlist/ordersview.php
@@ -0,0 +1,47 @@
+<?php
+
+if (!defined('ACCESS')) {
+    http_response_code(404);
+    die();
+}
+
+$body = <<<HTML
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 mt-4">
+                <a href="#" class="btn1"><i class="fas fa-chevron-left"></i>&nbsp;Back</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+
+            <div class="col-md-10">
+                <div class="content">
+                    <h2 class="table-title">My Orders</h2>
+
+                    <h2 class="history-title">Order History</h2>
+                    <div class="container mt-5">
+                        <div class="row">
+                            <div class="col-12">
+                                <table id="order-history" class="table table-striped table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>ORDERS</th>
+                                            <th>ORDER DATE</th>
+                                            <th>STATUS</th>
+                                            <th>TOTAL</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+HTML;
