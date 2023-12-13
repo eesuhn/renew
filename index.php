f378caca3850536224bf039334a6972251e6cedc
eesuhn <102596628+eesuhn@users.noreply.github.com>
2023-12-13 17:31:42 +0800

    Patched updates for v0.5
    1. Added `DataTable` view for multiple pages, included recycling log, artist products view
    2. Added dynamic navbar
    3. Refactor AJAX to allows image `FormData`
    4. Added most of the frontends required
    5. Added automation for sample loadup
    6. Added dynamic link chain for product focus

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * artists to showcase and sell their unique creations made from recycled materials.
  * 
  * @package ReNew
- * @version 0.1.0
+ * @version 0.5.0
  * @author 
  * - github.com/eesuhn
  * - github.com/shai-mohan
